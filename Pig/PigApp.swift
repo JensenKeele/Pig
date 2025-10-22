727096a06c8f4166ba324a50256c5968823daa92
jkeele <jkeele@bsd220.org>
2025-10-22 10:44:28 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Jensen Keele on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
